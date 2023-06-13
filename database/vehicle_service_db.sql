3e6e61af7df816bc3aa020fa05ba20b668f6759e
Abhi114320 <136404756+Abhi114320@users.noreply.github.com>
2023-06-13 07:32:57 +0530

    Add files via upload

---
+++ database/vehicle_service_db.sql
@@ -0,0 +1,292 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2021 at 10:40 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vehicle_service_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(30) NOT NULL,
+  `category` varchar(250) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `status`, `date_created`) VALUES
+(1, '2 Wheeler Vehicle', 1, '2021-09-30 09:42:40'),
+(2, '3 Wheeler Vehicle', 1, '2021-09-30 09:43:00'),
+(3, '4 Wheeler Vehicle', 1, '2021-09-30 09:43:48');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mechanics_list`
+--
+
+CREATE TABLE `mechanics_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mechanics_list`
+--
+
+INSERT INTO `mechanics_list` (`id`, `name`, `contact`, `email`, `status`, `date_created`) VALUES
+(1, 'RAhul', '09123456789', 'Rahul@sample.com', 1, '2021-09-30 10:26:11'),
+(2, 'Surya', '09112355799', 'surya@gmail.com', 1, '2021-09-30 10:30:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_meta`
+--
+
+CREATE TABLE `request_meta` (
+  `request_id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `request_meta`
+--
+
+INSERT INTO `request_meta` (`request_id`, `meta_field`, `meta_value`) VALUES
+(1, 'contact', '09112355799'),
+(1, 'email', 'rahul@sample.com'),
+(1, 'address', 'Sample Address'),
+(1, 'vehicle_name', 'Mitsubishi Montero Sport'),
+(1, 'vehicle_registration_number', 'AP 0623'),
+(1, 'vehicle_model', 'CDM-10140715'),
+(1, 'service_id', '1,3,4'),
+(1, 'pickup_address', 'Here St., There City, Sample Only 623');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_list`
+--
+
+CREATE TABLE `service_list` (
+  `id` int(30) NOT NULL,
+  `service` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `service_list`
+--
+
+INSERT INTO `service_list` (`id`, `service`, `description`, `status`, `date_created`) VALUES
+(1, 'Change Oil', '&lt;p&gt;&lt;span style=&quot;text-align: justify;&quot;&gt;An oil change is the process of removing and replacing the viscous fluid- otherwise known as oil- that runs through an internal combustion engine (aka the type of engine in a vehicle). The parts inside the engine move incredibly fast, which generates both friction and heat.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;',1,'2021-09-30 14:12:03'),
+(2, 'Overall Checkup', '&lt;p&gt;&lt;span style=&quot;text-align: justify;&quot;&gt;The basic processes of changing the oil, getting scheduled inspections of the car, and monitoring the tyre pressure are like the checkups that people do at a clinic. These checks are intended to keep the vehicle healthy and help the car owner or a mechanic catch serious issues before they turn into major problems.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;',1,'2021-08-30 14:12:03'),
+(3, 'Engine Tune up', '&lt;p&gt;&lt;span style=&quot;text-align: justify;&quot;&gt;An engine tune-up is the process of inspecting and replacing components of the engine that are essential to ignition and engine operation. A comprehensive tune-up can include: Replace spark plugs and wires/coil boots. Set ignition timing - if applicable. Replace engine air filter.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;',1,'2021-09-12 14:12:03'),
+(4, 'Tire Replacement', '&lt;p&gt;&lt;span style=&quot;text-align: justify;&quot;&gt;New tires with sufficient tread give your car maximum traction for accelerating and braking, as well as keeping a grip around curves on the road. However, the tread wears down every time you drive — even with proper maintenance — and tire replacement is necessary for safe driving . the tire replacement is also done in our services&lt;/span&gt;&lt;br&gt;&lt;/p&gt;',1,'2021-05-30 11:12:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_requests`
+--
+
+CREATE TABLE `service_requests` (
+  `id` int(30) NOT NULL,
+  `owner_name` text NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `service_type` text NOT NULL,
+  `mechanic_id` int(30) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `service_requests`
+--
+
+INSERT INTO `service_requests` (`id`, `owner_name`, `category_id`, `service_type`, `mechanic_id`, `status`, `date_created`) VALUES
+(1, 'Abhi', 3, 'Pick Up', 1, 2, '2021-09-30 14:48:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Vehicle Service Management System'),
+(6, 'short_name', 'VSMS - PHP'),
+(11, 'logo', 'uploads/1632965940_vrs-logo.jpg'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/1632965760_car-shop-clip.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1624240500_avatar.png', NULL, 1, '2021-01-20 14:02:37', '2021-06-21 09:55:07'),
+(6, 'Abhi', 'Ram', 'Aram', 'cd74fae0a3adf459f73bbf187607ccea', 'uploads/1632990840_ava.jpg', NULL, 2, '2021-09-30 16:34:02', '2021-09-30 16:35:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mechanics_list`
+--
+ALTER TABLE `mechanics_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `request_meta`
+--
+ALTER TABLE `request_meta`
+  ADD KEY `request_id` (`request_id`);
+
+--
+-- Indexes for table `service_list`
+--
+ALTER TABLE `service_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `service_requests`
+--
+ALTER TABLE `service_requests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `mechanics_list`
+--
+ALTER TABLE `mechanics_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `service_list`
+--
+ALTER TABLE `service_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `service_requests`
+--
+ALTER TABLE `service_requests`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `request_meta`
+--
+ALTER TABLE `request_meta`
+  ADD CONSTRAINT `request_meta_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `service_requests` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
